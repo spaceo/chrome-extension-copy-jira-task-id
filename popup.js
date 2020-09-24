61c09d581b7d4f1937a6d6798603a7a7daa2ba83
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2020-09-24 14:05:28 +0200

    Functionality works in full task view now as well

--- popup.js
+++ popup.js
@@ -8,20 +8,41 @@ function copyToClipboard(text) {
     document.body.removeChild(dummy);
 }
 
+function statusMessage(jiraTaskId) {
+    return `Jira task Id: <span class="what-was-copied">"${jiraTaskId}"</span> has been copied to your clipboard`;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const status = document.getElementById('status');
+    status.innerHTML = 'No project or task id was found';
+
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         const tabUrl = tabs[0].url;
-        const jiraTaskIdMatch = tabUrl.match(/selectedIssue=([A-Z][A-Z0-9]*-[0-9]+)/);
+        const projectAndTaskIdMatch = tabUrl.match(/browse\/([A-Z][A-Z0-9]*-[0-9]+)|selectedIssue=([A-Z][A-Z0-9]*-[0-9]+)/);
 
-        if (!jiraTaskIdMatch || jiraTaskIdMatch[1] === undefined) {
+        if (!projectAndTaskIdMatch) {
             status.innerHTML = 'No jira task id was found';
             return;
         }
 
-        const jiraTaskId = jiraTaskIdMatch[1];
-        copyToClipboard(jiraTaskId);
+        if (
+            projectAndTaskIdMatch
+            && projectAndTaskIdMatch[1] !== undefined
+            && projectAndTaskIdMatch[2] === undefined
+        ) {
+            copyToClipboard(projectAndTaskIdMatch[1]);
+            status.innerHTML = statusMessage(projectAndTaskIdMatch[1])
+        }
 
-        status.innerHTML = `Jira task Id: <span class="what-was-copied">"${jiraTaskId}"</span> has been copied to your clipboard`;
+        if (
+            projectAndTaskIdMatch
+            && projectAndTaskIdMatch[1] === undefined
+            && projectAndTaskIdMatch[2] !== undefined
+        ) {
+            copyToClipboard(projectAndTaskIdMatch[2]);
+            status.innerHTML = statusMessage(projectAndTaskIdMatch[2])
+        }
+
+        return true;
     });
 }, false);
